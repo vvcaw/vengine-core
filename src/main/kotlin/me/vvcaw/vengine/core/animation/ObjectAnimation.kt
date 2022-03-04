abf175a45f4c0755a88d0183ed4fd20ab70b2813
Eric Elflein <57096338+vvcaw@users.noreply.github.com>
2022-03-04 22:51:22 +0100

    Add function that allows animation

--- src/main/kotlin/me/vvcaw/vengine/core/animation/ObjectAnimation.kt
+++ src/main/kotlin/me/vvcaw/vengine/core/animation/ObjectAnimation.kt
@@ -6,6 +6,8 @@ import me.vvcaw.vengine.core.elements.Element
 data class ObjectAnimation<T : Element>(
     private val start: T,
     private val end: T,
+    private val elementPointer: T,
     val duration: Double,
     private val easing: Easing
 ) : Animation
+// TODO: Implement function to compare two states of an object and use start or end to do it (start.compare(start, end))
--- src/main/kotlin/me/vvcaw/vengine/core/elements/Ellipse.kt
+++ src/main/kotlin/me/vvcaw/vengine/core/elements/Ellipse.kt
@@ -1,9 +1,22 @@
 package me.vvcaw.vengine.core.elements
 
+import me.vvcaw.vengine.core.animation.Animation
+import me.vvcaw.vengine.core.animation.AnimationBuilder
 import processing.core.PApplet
 
-class Ellipse : Element {
+data class Ellipse(private var test: String) : Element {
     override fun render(app: PApplet) {
         app.ellipse(app.mouseX.toFloat(), app.mouseY.toFloat(), 20F, 20F)
+    }
+
+    // TODO: Make this an override method for clarity
+    fun animateTo(target: Ellipse): Animation {
+        // Build animation
+        val builder = AnimationBuilder(this.copy(), target.copy(), this)
+
+        // Update members, so that animation works when building the file
+        test = target.test
+
+        return builder.build()
     }
 }
