926534536c2e8f9c4a3ad277249a53415ec4c12e
Eric Elflein <57096338+vvcaw@users.noreply.github.com>
2022-03-04 22:51:51 +0100

    Add TODO

--- src/main/kotlin/me/vvcaw/vengine/core/elements/Element.kt
+++ src/main/kotlin/me/vvcaw/vengine/core/elements/Element.kt
@@ -3,5 +3,6 @@ package me.vvcaw.vengine.core.elements
 import processing.core.PApplet
 
 interface Element {
+    // TODO: Implement function to compare two states of an object
     fun render(app: PApplet)
 }
